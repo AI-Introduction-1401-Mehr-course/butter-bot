425b8f6073e0623bb1e1d4c419f3af997855208c
Mohammad Karami <1380mkkarami@gmail.com>
2022-11-11 01:12:43 +0330

    State space initial commit

---
+++ state_space.py
@@ -0,0 +1,26 @@
+
+from abc import ABC, abstractmethod
+from typing import NamedTuple, List, Type, Self
+from enum import Enum
+
+
+class StateSpace(ABC):
+    
+    Action = Type[Enum]
+
+    @property
+    @abstractmethod
+    def state(self) -> NamedTuple:
+        ...
+    
+    @abstractmethod
+    def result(self, action: Action) -> Self:
+        ...
+
+    @abstractmethod
+    def action(self) -> List[Action]:
+        ...
+    
+    @abstractmethod
+    def cost(self, action: Action) -> int:
+        ...
