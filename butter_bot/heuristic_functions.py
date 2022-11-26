33a25dcd984afe595a618e1b970f82bcbb7f6ffe
Mohammad Karami <1380mkkarami@gmail.com>
2022-11-27 00:10:07 +0330

    Implement new heuristics: - number_of_targets_with_no_butter - sum_of_min_cost_of_filling_target_cells_with_butter

--- butter_bot/heuristic_functions.py
+++ butter_bot/heuristic_functions.py
@@ -1,13 +1,54 @@
 from functools import partial
 
 from .butter_bot_state_space import ButterBotStateSpace
+from safe_typing import Cell, List
 
 
-def distance(cell_A, cell_B) -> int:
+def distance(cell_A: Cell, cell_B: Cell) -> int:
     """
     Returns distance between two cells.
     """
+
     return abs(cell_A[0] - cell_B[0]) + abs(cell_A[1] - cell_B[1])
+
+
+def min_cost_from_cell_matrix(
+    cell: Cell, cost_table: List[List[int]]
+) -> List[List[int]]:
+    """
+    Returns matrix of minimum cost requierd to reach any cell starting from `cell`.
+    """
+
+    ans: List[List[int]] = [[-1 for _ in i] for i in cost_table]
+    queue = [cell]
+    ans[cell[0]][cell[1]] = 0
+
+    while queue:
+        current_cell = queue.pop(0)
+        for new_cell in (
+            (current_cell[0] + i.value[0], current_cell[1] + i.value[1])
+            for i in ButterBotStateSpace.Action
+        ):
+            if (
+                0 <= new_cell[0] < len(ans)
+                and 0 <= new_cell[1] < len(ans[0])
+                and cost_table[new_cell[0]][new_cell[1]] != -1
+                and (
+                    ans[new_cell[0]][new_cell[1]] == -1
+                    or (
+                        ans[new_cell[0]][new_cell[1]]
+                        > ans[current_cell[0]][current_cell[1]]
+                        + cost_table[new_cell[0]][new_cell[1]]
+                    )
+                )
+            ):
+                ans[new_cell[0]][new_cell[1]] = (
+                    ans[current_cell[0]][current_cell[1]]
+                    + cost_table[new_cell[0]][new_cell[1]]
+                )
+                queue.append(new_cell)
+
+    return ans
 
 
 def bot_to_nearest_butter_that_is_nearest_to_target_to_nearest_target_heuristic(
@@ -75,6 +116,7 @@ def bot_to_nearest_butter_to_nearest_target_distance_heuristic(
     nearest_target = min(target_cells, key=partial(distance, nearest_butter))
     return distance(bot_cell, nearest_butter) + distance(nearest_butter, nearest_target)
 
+
 def number_of_butters_not_on_a_target(
     state_space: ButterBotStateSpace,
 ) -> float:
@@ -87,3 +129,34 @@ def number_of_butters_not_on_a_target(
         cell for cell in state.butter_cells if cell not in state.target_cells
     ]
     return len(butter_cells)
+
+
+def number_of_targets_with_no_butter(state_space: ButterBotStateSpace) -> float:
+
+    state = state_space.state
+    return sum(1 for cell in state.target_cells if cell not in state.butter_cells)
+
+
+# Heuristics derived from relaxed versions of the problem:
+
+
+def sum_of_min_cost_of_filling_target_cells_with_butter(
+    state_space: ButterBotStateSpace,
+) -> float:
+    """
+    Teleporting bot with ability of picking up butters.
+    Also duplicating butters and walking through them and stuff...
+    """
+
+    state = state_space.state
+    return sum(
+        min(
+            cost_matrix[butter_cell[0]][butter_cell[1]]
+            for butter_cell in state.butter_cells
+        )
+        for cost_matrix in (
+            min_cost_from_cell_matrix(cell, state.cost_table)
+            for cell in state.target_cells
+            if cell not in state.butter_cells
+        )
+    )
